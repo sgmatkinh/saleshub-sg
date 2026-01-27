8aa4d37cfd179c2712736751406d3b4d72253799
sgmatkinh <sgmatkinh@gmail.com>
2026-01-27 11:53:26 +0700

    Update POS.jsx

--- src/features/POS.jsx
+++ src/features/POS.jsx
@@ -79,9 +79,9 @@ export default function POS() {
       note: orderData.note || "Không có"
     };
 
-    const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
-    const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
-    const publicKey = process.env.REACT_APP_EMAILJS_PUBLIC_KEY;
+    const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+    const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+    const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
 
     if (serviceId && templateId && publicKey) {
         emailjs.send(serviceId, templateId, templateParams, publicKey)
